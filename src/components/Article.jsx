7c3c8bad9889170090d80294897f58e559c1843d
Victor Mwaura Mwangi <victor.mwangi6@student.moringaschool.com>
2026-05-02 23:58:16 +0300

    EXACT component files with .jsx extensions

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,17 +3,13 @@ import React from "react";
 function Article({ title, date = "January 1, 1970", preview, minutes }) {
   const minutesColor = minutes < 30 ? "green" : "red";
   const minutesDisplay = minutes < 30 ? "lightning" : "long";
-
+  
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
       <p>{preview}</p>
-      <p>
-        <span style={{ color: minutesColor }}>
-          ● {minutes} min {minutesDisplay} read
-        </span>
-      </p>
+      <p><span style={{color: minutesColor}}>● {minutes} min {minutesDisplay} read</span></p>
     </article>
   );
 }
